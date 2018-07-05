ef05038afaa4b15f8c3e860a88169c8275041d99
446734796@qq.com <wuqiang502>
2018-07-05 21:32:08 +0800

    first commit

---
+++ src/main/java/com/northbrain/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.northbrain.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+	//our
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
